be59fd486468513842b14af5ef8f8d793d19cd2a
hossam ameen <hosamameen948@gmail.com>
2023-02-27 20:52:42 +0200

    add load fixture file

---
+++ delivary/load_fixtures.py
@@ -0,0 +1,4 @@
+# python3 manage.py loaddata fixtures/users.json
+# python3 manage.py dumpdata users --indent 4 > fixtures/users.json
+import os
+os.system("python3 manage.py loaddata fixtures/users.json")
